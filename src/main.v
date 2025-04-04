afc42fc29d35aeac8d28682c5bccb791345368dc
André Luiz <andreluiz.ddev@gmail.com>
2025-04-04 07:06:23 -0300

    fix time sleep to target

--- src/main.v
+++ src/main.v
@@ -10,9 +10,22 @@ fn main() {
 		current_time := time.now()
 
 		current_date := current_time.custom_format('YYYY-MM-DD')
-		serven_hour_from_tomorrow := time.parse('${current_date} 00:07:00') or { time.now() }.add_days(1)
+		current_hour := current_time.hour
+		
+		mut serven_hour_today := time.parse('${current_date} 07:00:00') or { time.now() }
+		
+		// Se a hora atual for menor que 8:00, define para as 8:00 do mesmo dia
+		// Se a hora atual for maior ou igual a 8:00, define para as 8:00 do dia seguinte
+		if current_hour >= 8 {
+			serven_hour_today = serven_hour_today.add_days(1)
+		}
 
-		wait_time := serven_hour_from_tomorrow - current_time
+		mut wait_time := serven_hour_today - current_time
+
+		if wait_time < 0 {
+			serven_hour_today = serven_hour_today.add_days(1)
+			wait_time = serven_hour_today - current_time
+		}
 
 		println('Aguardando ${wait_time}s até o próximo envio...')
 
@@ -23,7 +36,7 @@ fn main() {
 			dump('Env Dev')
 		}
 
-		services.send_recomendations()
+		// services.send_recomendations()
 
 		$if debug? {
 			time.sleep(time.minute * 2)
